fd14c410eba2a5a09debeecf3a6e9aadb657ff6b
Maeva DTC <delruemaeva@orange.fr>
2024-08-22 14:50:12 +0200

    undone

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,23 @@ import "./App.css";
 
 const App = () => {
   // const [count, setCount] = useState(0);
-  const [isActive, setIsActive] = useState(true);
+  const [isActive1, setIsActive1] = useState(true);
+  const [isActive2, setIsActive2] = useState(true);
+  const [isActive3, setIsActive3] = useState(true);
   const [status, setStatus] = useState("No way!");
 
-  const handleSwitch = () => {
-    setIsActive(!isActive);
+  const handleSwitch1 = () => {
+    setIsActive1(!isActive1);
+  };
+  const handleSwitch2 = () => {
+    setIsActive2(!isActive2);
+  };
+  const handleSwitch3 = () => {
+    setIsActive3(!isActive3);
+  };
+
+  const changeText = () => {
+    setStatus("Go!");
   };
 
   return (
@@ -19,48 +31,59 @@ const App = () => {
         <aside>
           <div>
             <button
-              className={isActive ? "active" : "not-active"}
-              onClick={handleSwitch}
+              className={isActive1 ? "active" : "not-active"}
+              onClick={!isActive1 ? handleSwitch1 : null}
+              // onClick={() => {
+              //   if (!isActive1) {
+              //     handleSwitch1();
+              //   } else {
+              //     return;
+              //   }
+              // }}
             >
               On
             </button>
             <button
-              className={isActive ? "active" : "not-active"}
-              onClick={handleSwitch}
+              className={isActive2 ? "active" : "not-active"}
+              onClick={handleSwitch2}
             >
               On
             </button>
             <button
-              className={isActive ? "active" : "not-active"}
-              onClick={handleSwitch}
+              className={isActive3 ? "active" : "not-active"}
+              onClick={handleSwitch3}
             >
               On
             </button>
           </div>
           <div>
             <button
-              className={isActive ? "active" : "not-active"}
-              onClick={handleSwitch}
+              className={isActive1 ? "not-active" : "active"}
+              onClick={handleSwitch1}
             >
               Off
             </button>
             <button
-              className={isActive ? "active" : "not-active"}
-              onClick={handleSwitch}
+              className={isActive2 ? "not-active" : "active"}
+              onClick={handleSwitch2}
             >
               Off
             </button>
             <button
-              className={isActive ? "active" : "not-active"}
-              onClick={handleSwitch}
+              className={isActive3 ? "not-active" : "active"}
+              onClick={handleSwitch3}
             >
               Off
             </button>
           </div>
         </aside>
-        <button>
-          <span>{status}</span>
-        </button>
+        <span
+          className={
+            isActive1 && isActive2 && isActive3 ? "statusOK" : "statusNO"
+          }
+        >
+          {/* {isActive1 && isActive2 && isActive3 ? changeText : status} */}
+        </span>
       </main>
     </>
   );
